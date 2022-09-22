24fdca74ab4d07f2a6b5b94c3234bdfbe62e1ec7
Ngoni19 <ngoni19@live.com>
2022-09-22 22:12:47 +0000

    dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
                  "arguments" if (size) != 1 else "argument",
                  "." if (size) == 0 else ":"))
     for idx, arg in enumerate(userin):
-        print("{:d}: {:s}".format(idx + 1, arg)) 
+        print("{:d}: {:s}".format(idx + 1, arg))
