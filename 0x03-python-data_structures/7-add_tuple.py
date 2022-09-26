ad668393878a2694bc0c46268301edf20027e189
Ngoni19 <ngoni19@live.com>
2022-09-26 22:58:12 +0000

    tuple addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,6 +5,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     y = len(tuple_b)
 
     t_add = ((tuple_a[0] if x > 0 else 0) + (tuple_b[0] if y > 0 else 0),
-            (tuple_a[1] if x > 1 else 0) + (tuple_b[1] if y > 1 else 0))
+             (tuple_a[1] if x > 1 else 0) + (tuple_b[1] if y > 1 else 0))
 
     return t_add
