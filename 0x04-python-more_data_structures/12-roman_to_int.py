09eb23da55bbc14df86dfde7fde25d0109c2ac9a
Ngoni19 <ngoni19@live.com>
2022-09-30 09:35:41 +0000

    Roman numerals to int funct

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,13 +5,13 @@ def roman_to_int(roman_string):
     if roman_string == "":
         return 0
     n = 0
-    rom_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    rom_d = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     for k, j in zip(roman_string, roman_string[1:]):
-        if k not in rom_dict.keys():
+        if k not in rom_d.keys():
             return 0
-        elif rom_dict[k] >= rom_dict[j]:
-            n += rom_dict[k]
+        elif rom_d[k] >= rom_d[j]:
+            n += rom_d[k]
         else:
-            n -= rom_dict[k]
-    n += rom_dict[roman_string[-1]]
+            n -= rom_d[k]
+    n += rom_d[roman_string[-1]]
     return n
