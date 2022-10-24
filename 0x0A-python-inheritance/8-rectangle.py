4457812ed66272809f42b5f1e72d439477dc56c2
Ngoni19 <ngoni19@live.com>
2022-10-24 20:24:35 +0000

    rectangle with inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,12 +1,12 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 """
 Module 8-rectangle
 
-@parent class BaseGeometry
+Contains parent class BaseGeometry
 with public instance method area & integer_validator
 
-@subclass Rectangle
-with instantiation of __attributes width & height, validated by parent
+Contains subclc Rectangle
+with instantiation of priv attri width & height, validated by parent
 """
 
 
@@ -14,12 +14,12 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """inherits from BaseGeometry
+    """inherits from BaseGeometry clc
     Methods:
         __init__(self, width, height)
     """
     def __init__(self, width, height):
-        """validate; initialize width & height
+        """validate & initialize width & height
         Args:
             width (int): private
             height (int): private
