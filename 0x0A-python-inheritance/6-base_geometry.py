1fdba6ae539dc63bae82303dd73ba36fdb85af70
Ngoni19 <ngoni19@live.com>
2022-10-24 19:23:16 +0000

    raise exception funct

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+
+@class BaseGeometry - > empty
+with public instance method area
+"""
+
+
+class BaseGeometry:
+    """
+    Methods:
+        area(self)
+    """
+    def area(self):
+        """has exceptions; area not implemented"""
+        raise Exception("area() is not implemented")
