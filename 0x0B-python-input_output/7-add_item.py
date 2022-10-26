f26bd2e9d41516af874e0126b6aee1722ae320bc
Ngoni19 <ngoni19@live.com>
2022-10-26 20:02:25 +0000

    script using argv and json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -17,8 +17,8 @@ Module 7-add_item
 
 
 from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
