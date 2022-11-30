06220d879c8bb687fa936524d0236474cce8860c
Ngonidzashe Brandon Towindo <106912178+Ngoni19@users.noreply.github.com>
2022-12-01 01:18:41 +0200

    Update 5-unique_id.sql

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
--- creates the table unique_id
--- description of table data: id INT with the default value 1, must be unique & name VARCHAR(256)
+-- script creates the table unique_id on MySQL server
+-- table data descri: id INT with the default value 1, must be unique & name VARCHAR(256)
 -- should not fail if exists
 
 CREATE TABLE IF NOT EXISTS unique_id (
