0ccde0726cd3ca51948302f6981b1dfdad8c879c
Ngonidzashe Brandon Towindo <106912178+Ngoni19@users.noreply.github.com>
2022-12-01 01:31:15 +0200

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,14 +1,14 @@
--- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
--- run this first to import a SQL dump -->
+--import https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
+--import a SQL dump -->
 --      echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p
 --      curl [link] -s | mysql -uroot -p hbtn_0d_tvshows
 --
--- script that lists all shows contained in the database hbtn_0d_tvshows
+-- script lists all shows contained in the database hbtn_0d_tvshows
 -- each record should display: tv_shows.title - tv_show_genres.genre_id
 -- results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- if a show doesn’t have a genre, display NULL
 -- can use only one SELECT statement
 -- the database name will be passed as an argument of the mysql command
--- if a show doesn’t have a genre, display NULL
 
    SELECT tv_shows.title, tv_show_genres.genre_id
      FROM tv_shows
