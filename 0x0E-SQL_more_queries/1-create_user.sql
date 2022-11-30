968c00c3ad1df8cc3c8703be93fedd399810ea92
Ngonidzashe Brandon Towindo <106912178+Ngoni19@users.noreply.github.com>
2022-12-01 01:11:46 +0200

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- creates the MySQL server user user_0d_1
--- should have all privileges
--- password should be set to user_0d_1_pwd
+-- user_0d_1 should have all privileges on MySQL server
+-- user_0d_1 password should be set to user_0d_1_pwd
 -- if user_0d_1 already exists, script should not fail
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
