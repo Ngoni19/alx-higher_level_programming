33736b3398eef2747f82668cbe5aedb8b18a7fa1
Ngonidzashe Brandon Towindo <106912178+Ngoni19@users.noreply.github.com>
2022-12-01 01:15:44 +0200

    Update 3-force_name.sql

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
--- creates the table force_name
--- data in table: id INT, name VARCHAR(256) can’t be null
+-- script creates the table force_name on MySQL server
+-- data insert into table: id INT, name VARCHAR(256) can’t be null
 -- database name will be passed as an argument of the mysql command
 -- if table exists, script should not fail
 
