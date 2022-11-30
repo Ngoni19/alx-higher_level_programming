8c4bcddfd1ad8406867b8fdb717b91111bd07783
Ngonidzashe Brandon Towindo <106912178+Ngoni19@users.noreply.github.com>
2022-12-01 01:34:46 +0200

    Update 14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- import https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
--- run this first to import a SQL dump -->
---      echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p
---      curl [link] -s | mysql -uroot -p hbtn_0d_tvshows
+-- import a SQL dump -->
+--     echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p
+--     curl [link] -s | mysql -uroot -p hbtn_0d_tvshows
 --
 -- script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
 -- tv_shows table contains only one record where title = Dexter (but the id can be different)
