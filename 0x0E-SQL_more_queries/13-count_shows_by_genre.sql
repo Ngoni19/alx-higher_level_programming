7859e0e5ed784d90da0b7562220668d3e80010bf
Ngonidzashe Brandon Towindo <106912178+Ngoni19@users.noreply.github.com>
2022-12-01 01:33:43 +0200

    Update 13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
--- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
--- run this first to import a SQL dump -->
+-- import https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
+-- import a SQL dump ->
 --      echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p
 --      curl [link] -s | mysql -uroot -p hbtn_0d_tvshows
 --
