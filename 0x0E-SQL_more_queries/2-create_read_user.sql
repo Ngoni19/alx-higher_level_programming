003e0501d62c7fcb72a0838461b7b4853c982177
Ngonidzashe Brandon Towindo <106912178+Ngoni19@users.noreply.github.com>
2022-12-01 01:14:08 +0200

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
--- creates the database hbtn_0d_2 and the user user_0d_2
+-- script creates the database hbtn_0d_2 and the user user_0d_2.
 -- user_0d_2 password should be set to user_0d_2_pwd
--- should not fail if either already exists
+-- should not fail if either already exists in database
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
