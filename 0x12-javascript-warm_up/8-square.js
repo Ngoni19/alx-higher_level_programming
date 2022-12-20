0424d35a357b04954b1c11750f876e81b5607591
Ngonidzashe Brandon Towindo <ngoni@live.com>
2022-12-20 23:20:20 +0200

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 
 const Size = parseInt(process.argv[2], 10);
 if (isNaN(Size)) {
-  console.log('Missing Size');
+  console.log('Missing size');
 } else {
   for (let j = 0; j < Size; j += 1) {
     console.log('X'.repeat(Size));
