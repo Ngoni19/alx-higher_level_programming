ac241eb57685d4b6dbc4a747fbdf08e5236b3b6a
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-02-10 21:42:43 +0200

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let file_name = process.argv[2];
+let filename = process.argv[2];
 const fs = require('fs');
-fs.readFile(file_name, 'utf8', function (err, data) {
+fs.readFile(filename, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
