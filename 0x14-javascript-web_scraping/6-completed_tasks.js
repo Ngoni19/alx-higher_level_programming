476f26c22d9f4efb5e0917929c5b78e01e3c53ef
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-02-10 22:41:23 +0200

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,24 +1,24 @@
 #!/usr/bin/node
 
-let url = process.argv[2];
-const req = require('req');
+let URL = process.argv[2];
+const request = require('request');
 
-req(url, function (err, response, body) {
+request(URL, function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
-    let d01 = {};
+    let dict = {};
     let no_tasks = JSON.parse(body);
     for (let j in no_tasks) {
       if (no_tasks[j].completed) {
-	if (d01[no_tasks[j].userId] === undefined) {
-	  d01[no_tasks[j].userId] = 1;
+	if (dict[no_tasks[j].userId] === undefined) {
+	  dict[no_tasks[j].userId] = 1;
 	} else {
-	  d01[no_tasks[j].userId]++;
+	  dict[no_tasks[j].userId]++;
 	}
       }
     }
-    console.log(d01);
+    console.log(dict);
   } else {
     console.log('Error code: ' + response.statusCode);
   }
